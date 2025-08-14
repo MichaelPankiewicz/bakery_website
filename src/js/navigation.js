1a07f753b371aa8eb4f35061237ac1c4e4acb9fe
Michal Pankiewicz <m.pankiewicz@s-bb.nl>
2025-08-14 17:24:30 +0200

    fixed api for topMenu

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -82,16 +82,16 @@ document.addEventListener('DOMContentLoaded', () => {
         });
 
         // Fetch menu items array
+
+        const baseUrl = process.env.NODE_ENV === 'development'
+            ? 'http://localhost:3000'
+            : '';
+
+        const url = process.env.NODE_ENV === 'development'
+            ? `${baseUrl}/topMenu`
+            : `${baseUrl}/api/topMenu`;
+            
         try {
-
-
-            const baseUrl = process.env.NODE_ENV === 'development'
-                ? 'http://localhost:3000'
-                : '';
-
-            const url = process.env.NODE_ENV === 'development'
-                ? `${baseUrl}/topMenu`
-                : `${baseUrl}/api/topMenu`;
 
             const response = await fetch(url);
             menuItems = await response.json();
